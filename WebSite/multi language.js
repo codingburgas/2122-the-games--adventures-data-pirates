afd3bfa26fc88e61a1ddc6c0c0a94a1125116a1d
GGDimitrov21 <GGDimitrov21@codingburgas.bg>
2022-06-12 18:04:43 +0300

    Changing the js data

---
+++ WebSite/multi language.js
@@ -0,0 +1,12 @@
+// define language anchors
+var dataReload = document.querySelectorAll("[data-reload]");
+
+// Language translations
+var language = {
+    eng: {
+      welcome: "Welcome everyone!"
+    }, 
+    bg: {
+      welcome: "Здравейте всички!"
+    }
+};
